a679189534b516da8db92e2d7aa0c4989e3a6dd1
Kingshuk Majumder <kingshukm2021@gmail.com>
2023-01-19 23:17:41 +0530

    modified

--- mongodb.js
+++ mongodb.js
@@ -3,12 +3,23 @@ const url='mongodb://localhost:27017';
 dbname='e-comm';
 const client=new MongoClient(url);
 
-async function getData(){
+async function dbConnect(){
     let conn=await client.connect();
     let db=conn.db(dbname);
-    let collection=db.collection('products');
-    let result=await collection.find({item:"M-30S"}).toArray();
-    console.log(result);
+   return db.collection('products');
 }
 
-getData();
+// dbConnect().then((resp)=>{
+//     resp.find({}).toArray().then((data)=>{
+//         console.log(data);
+//     })
+// });
+let main=async ()=>{
+    let data=await dbConnect();
+    data=await data.find().toArray();
+    console.log(data);
+
+}
+
+
+main();
