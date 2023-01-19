d53583c2ba445f2bed504dea372a296c593c801e
Kingshuk Majumder <kingshukm2021@gmail.com>
2023-01-19 21:41:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const {MongoClient} = require('mongodb')
+const url='mongodb://localhost:27017';
+const dbName='e-comm'
+const client=new MongoClient(url);
+
+async function getData(){
+    let result=await client.connect();
+    let db=result.db(dbName);
+    let collection=db.collection('products');
+    let response=await collection.find({}).toArray();
+    console.log(response);
+}
+getData();
