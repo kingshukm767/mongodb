eb75736988a4af17a824d931bb9ca00d680835da
Kingshuk Majumder <kingshukm2021@gmail.com>
2023-01-20 11:41:29 +0530

    modified

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const {MongoClient} = require('mongodb')
-const url='mongodb://localhost:27017';
-const dbName='e-comm'
-const client=new MongoClient(url);
+const dbConnect=require('./mongodb');
+// const {MongoClient} = require('mongodb')
+// const url='mongodb://localhost:27017';
+// const dbName='e-comm'
+// const client=new MongoClient(url);
 
-async function getData(){
-    let result=await client.connect();
-    let db=result.db(dbName);
-    let collection=db.collection('products');
-    let response=await collection.find({}).toArray();
-    console.log(response);
+// async function getData(){
+//     let result=await client.connect();
+//     let db=result.db(dbName);
+//     let collection=db.collection('products');
+//     let response=await collection.find({}).toArray();
+//     console.log(response);
+// }
+// getData();
+
+let main=async ()=>{
+    let data=await dbConnect();
+    data=await data.find().toArray();
+    console.log(data);
+
 }
-getData();
+
+
+main();
--- mongodb.js
+++ mongodb.js
@@ -14,12 +14,13 @@ async function dbConnect(){
 //         console.log(data);
 //     })
 // });
-let main=async ()=>{
-    let data=await dbConnect();
-    data=await data.find().toArray();
-    console.log(data);
+// let main=async ()=>{
+//     let data=await dbConnect();
+//     data=await data.find().toArray();
+//     console.log(data);
 
-}
+// }
 
 
-main();
+// main();
+module.exports=dbConnect;
